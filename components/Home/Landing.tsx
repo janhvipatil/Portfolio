2aadc842bf7d5eb23f82fc0d0951d9070707eefd
Janhvi <janhvipatil716@gmail.com>
2022-09-27 16:29:14 +0530

    added lottie animation

--- components/Home/Landing.tsx
+++ components/Home/Landing.tsx
@@ -1,10 +1,30 @@
-import { Box } from '@chakra-ui/react'
+import { Box, HStack } from '@chakra-ui/react'
 import { MainContent } from '../LandingUI'
+import Lottie from 'react-lottie'
+import animationData from '../../assets/animations/anime.json'
 
 export const Landing = () => {
+
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+        }
+    }
+
     return (
-        <Box>
-            <MainContent />
-        </Box>
+        <>
+            <HStack display={{ base: 'none', md: 'flex' }}>
+                <MainContent />
+                <Box>
+                    <Lottie options={defaultOptions} height={800} width={400} />
+                </Box>
+            </HStack>
+            <Box display={{ base: 'flex', md: 'none' }}>
+                <MainContent />
+            </Box>
+        </>
     )
 }
