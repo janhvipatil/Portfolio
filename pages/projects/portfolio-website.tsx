de262ef34e32f5f5322cf6ec428238ade5817f24
Janhvi Patil <janhvipatil716@gmail.com>
2022-12-22 16:20:29 +0530

    added project info

--- pages/projects/portfolio-website.tsx
+++ pages/projects/portfolio-website.tsx
@@ -1,10 +1,12 @@
-import { Avatar, Box, Button, Divider, Flex, Heading, HStack, Stack, Tag, Text } from '@chakra-ui/react'
+import {Box, Button, Divider, Flex, Heading, HStack, Stack, Tag, Text } from '@chakra-ui/react'
 import React from 'react'
 import { CoverImage } from '../../components/CoverImage/CoverImage'
 import project5 from '../../assets/projects/portfolio-cover.png'
+import Image from 'next/image'
 import Link from 'next/link'
 import { AiOutlineArrowRight, AiOutlineCalendar } from 'react-icons/ai'
 import { Footer } from '../../components/Footer'
+import avatar from '../../assets/about/avatar.png'
 
 type Props = {}
 
@@ -55,8 +57,7 @@ const Project5 = (props: Props) => {
                                 <Stack pl={{ base: 10, md: 0 }}>
                                     <Heading fontSize={{ base: '4xl', md: '5xl' }}>Portfolio Website</Heading>
                                     <HStack>
-                                        <Tag>tag 1</Tag>
-                                        <Tag>tag 2</Tag>
+                                        <Tag>Personal project</Tag>
                                     </HStack>
                                 </Stack>
                             </Stack>
@@ -66,79 +67,29 @@ const Project5 = (props: Props) => {
                                 <Box w='70%'>
                                     <DetailsStack heading='overview' stackSpacing={4}>
                                         <Text letterSpacing='wide'>
-                                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-                                        </Text>
-                                        <Text letterSpacing='wide'>
-                                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-                                        </Text>
-                                        <Text letterSpacing='wide'>
-                                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+                                        As a web developer, I am always looking for new challenges and ways to showcase my skills. That&apos;s why I was excited to design and create my own portfolio website using NextJS, TypeScript, and Chakra UI. The site not only showcases my work and bio, but it also includes some fun easter eggs for users to discover. For example, when you inspect the site, you&apos;ll find a personal message from me. I&apos;ve also included links to my latest CV and some of my favorite projects, complete with GitHub links and preview links so that visitors can see my work in action. 
                                         </Text>
                                     </DetailsStack>
                                 </Box>
                                 <Stack spacing={4}>
                                     <DetailsStack heading='collaborators' stackSpacing={2}>
-                                        <HStack>
-                                            <HStack>
-                                                <Avatar
-                                                    size='xs'
-                                                    name='Kola Tioluwani'
-                                                    src='https://bit.ly/tioluwani-kolawole' />
-                                                <Text>Person 1</Text>
-                                            </HStack>
-                                            <HStack>
-                                                <Avatar
-                                                    size='xs'
-                                                    name='Kola Tioluwani'
-                                                    src='https://bit.ly/tioluwani-kolawole' />
-                                                <Text>Person 2</Text>
-                                            </HStack>
+                                    <HStack>
+                                            <Image src={avatar} alt='my picture' height='24px' width='24px' />
+                                            <Text>Janhvi Patil</Text>
                                         </HStack>
                                     </DetailsStack>
                                     <DetailsStack heading='my role' stackSpacing={2}>
-                                        <Text>Role 1, Role 2, Role 3</Text>
+                                        <Text>Developer</Text>
+                                        <Text>Designer</Text>
                                     </DetailsStack>
                                     <DetailsStack heading='duration' stackSpacing={2}>
-                                        <HStack><AiOutlineCalendar /><Text fontSize='sm'>Jun 2016 to Feb 2019</Text></HStack>
+                                        <HStack><AiOutlineCalendar /><Text fontSize='sm'>September 2022 to December 2022</Text></HStack>
                                     </DetailsStack>
                                 </Stack>
                             </Stack>
 
                         </Stack>
 
-                        {/** Process Information */}
-                        <Stack spacing={16} pl={{ base: 10, md: 16 }} pr={{ base: 12, md: 24 }}>
-
-                            <Stack spacing={6}>
-                                <PhaseHeading heading='Understanding the problem' />
-                                <Text letterSpacing='wide'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</Text>
-                            </Stack>
-
-                            <Stack spacing={6}>
-                                <PhaseHeading heading='Gathering insights' />
-                                <Text letterSpacing='wide'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</Text>
-                            </Stack>
-
-                            <Stack spacing={6}>
-                                <PhaseHeading heading='Solution architecting' />
-                                <Text letterSpacing='wide'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</Text>
-                            </Stack>
-
-                            <Stack spacing={6}>
-                                <PhaseHeading heading='Wireframing' />
-                                <Text letterSpacing='wide'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</Text>
-                            </Stack>
-
-                            <Stack spacing={6}>
-                                <PhaseHeading heading='Development' />
-                                <Text letterSpacing='wide'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</Text>
-                            </Stack>
-
-                            <Stack spacing={6}>
-                                <PhaseHeading heading='Results and Takeaways' />
-                                <Text letterSpacing='wide'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</Text>
-                            </Stack>
-                        </Stack>
                     </Stack>
                     <Divider />
                     <Flex justifyContent='space-between' pr={{ base: 5 }}>
